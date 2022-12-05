035fb4cca202cb7fe61c69a88e6af8e56f0982b4
felosc <49010129+felosc@users.noreply.github.com>
2022-12-05 11:42:52 -0500

    se agrego jquery para generar cierre automatico de las alertas en word.index

--- resources/views/word/index.blade.php
+++ resources/views/word/index.blade.php
@@ -8,12 +8,12 @@
     
     <div class="max-w-lg mx-auto mt-5 text-center">
     @if (session('success'))
-    <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
+    <div id="alert-dismissible" class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
         <span class="font-medium">{{ session('success') }}</span>
     </div>
         @endif
         @if (session('fail'))
-        <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+        <div id="alert-dismissible" class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
             <span class="font-medium">{{ session('fail') }}</span>
         </div>
         @endif
@@ -36,6 +36,19 @@
 @endforeach
 
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        $("#alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+            $("#alert-dismissible").alert('close');
+        });
+
+        $('[data-toggle="tooltip"]').tooltip({
+            trigger : 'hover'
+        });
+    });
+</script>
 </x-app-layout>
 
 
