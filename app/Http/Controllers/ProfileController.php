b99c170daeb429ca786855e305c9ae1da695349a
felosc <49010129+felosc@users.noreply.github.com>
2023-03-20 16:43:02 -0500

    se agrego alerta en profile.edit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -36,9 +36,11 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        if ($request->user()->save() == true) {
+            return Redirect::route('profile.edit')->with('success', 'profile-updated');
+        } else {
+            return Redirect::route('profile.edit')->with('success', 'fail-profile-updated');
+        };
     }
 
     /**
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,6 +5,19 @@
         </h2>
     </x-slot>
 
+        <div class="max-w-lg mx-auto mt-5 text-center">
+    @if (session('success'))
+    <div id="alert-dismissible" class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
+        <span class="font-medium">{{ session('success') }}</span>
+    </div>
+        @endif
+        @if (session('fail'))
+        <div id="alert-dismissible" class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+            <span class="font-medium">{{ session('fail') }}</span>
+        </div>
+        @endif
+    </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
@@ -26,4 +39,21 @@
             </div>
         </div>
     </div>
+
+
+<!-- utilidades y scripts para que la alerda desaparezca sola--->
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        $("#alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+            $("#alert-dismissible").alert('close');
+        });
+
+        $('[data-toggle="tooltip"]').tooltip({
+            trigger : 'hover'
+        });
+    });
+</script>
+
 </x-app-layout>
