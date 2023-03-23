b45820473a76c032a3b6fc291f3943e86026dbb9
felosc <49010129+felosc@users.noreply.github.com>
2023-03-23 10:24:16 -0500

    CAMBIO EN EL ESTILO  DE LA TABAL EN USER.INDEX

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -10,30 +10,30 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
         <div class="flex items-center justify-center">     
-          <table class="border border-slate-900">
+          <table class="bg-gray-200 rounded-lg border-separate border-spacing-2">
             <thead>
               <tr >
-                  <th class="border border-slate-900 ... bg-slate-200">NAME id</th>
-                  <th class="border border-slate-900 ... bg-slate-200">NAME USER</th>
-                  <th class="border border-slate-900 ... bg-slate-200">EMAIL USER</th>
-                  <th class=" border-slate-900 ... bg-slate-200" colspan="3">Actions</th>
+                  <th class="">NAME ID</th>
+                  <th class="">NAME USER</th>
+                  <th class="">EMAIL USER</th>
+                  <th class="" colspan="3">ACTIONS</th>
                 </tr>
             </thead>
             <tbody>    
               @foreach ($users as $user )
                
               <tr>
-                  <td class="bg-cyan-200 border border-slate-700 ... text-center">{{$user->id}}</td>
-                  <td class="bg-cyan-200 border border-slate-700 ... text-center">{{$user->name}}</td>
-                  <td class="bg-red-200 border border-slate-700 ... text-center">{{$user->email}}</td>              
-                    <td class="bg-indigo-200 border border-slate-700 ... text-center">                   
+                  <td class="text-center">{{$user->id}}</td>
+                  <td class="text-center">{{$user->name}}</td>
+                  <td class="">{{$user->email}}</td>              
+                    <td class="">                   
                           <a href="{{route('user.show', $user->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-0.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Show</a>
                   </td>
-                          <td class="bg-indigo-200 border border-slate-700 ... text-center">
+                          <td class="">
                             
                             <a href="{{route('user.edit', $user->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-0.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</a>
                           </td>
-                          <td class="bg-indigo-200 border border-slate-700 ... text-center">
+                          <td class="">
                             <form action="{{ route('user.delete',$user->id) }}" method="POST">
                               @csrf
                               @method('DELETE')
